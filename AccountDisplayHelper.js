62318941b9c4518476db0288b9b062acc57f303c
raydisharee <47063502+raydisharee@users.noreply.github.com>
2019-01-26 23:43:39 +0530

    Create AccountDisplayHelper.js

---
+++ AccountDisplayHelper.js
@@ -0,0 +1,10 @@
+({
+	fetchRecentAccount: function(component) {
+        var action = component.get('c.getAccounts');
+        var self = this;
+        action.setCallback(this, function(actionResult) {
+         component.set('v.accounts', actionResult.getReturnValue());
+        });
+        $A.enqueueAction(action);
+      }
+})
